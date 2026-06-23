53f34730233049e0face38527fb4b61553bf990a
wafiperso <wafibenjeddou@gmail.com>
2026-06-23 23:45:54 +0100

    Updates

---
+++ precache-manifest.ad8bc554afd44a80895b165567131835.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0135e16a79e19c5e16fd990164b7f356",
+    "url": "/Car-App-mern/index.html"
+  },
+  {
+    "revision": "5ae6026debd097663088",
+    "url": "/Car-App-mern/static/css/2.8a1a4a43.chunk.css"
+  },
+  {
+    "revision": "b1d20556e6db2dd6c62e",
+    "url": "/Car-App-mern/static/css/main.4121fb9d.chunk.css"
+  },
+  {
+    "revision": "5ae6026debd097663088",
+    "url": "/Car-App-mern/static/js/2.d5227809.chunk.js"
+  },
+  {
+    "revision": "b7ca5037ee8b50998b911751f7cce6fd",
+    "url": "/Car-App-mern/static/js/2.d5227809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16430a8b710ad3e3daa1",
+    "url": "/Car-App-mern/static/js/3.cd8cf101.chunk.js"
+  },
+  {
+    "revision": "b1d20556e6db2dd6c62e",
+    "url": "/Car-App-mern/static/js/main.98e5fae8.chunk.js"
+  },
+  {
+    "revision": "9a7ac08ba4a828d2e93a",
+    "url": "/Car-App-mern/static/js/runtime-main.7fee730c.js"
+  }
+]);
